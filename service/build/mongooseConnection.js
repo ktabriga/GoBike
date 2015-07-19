359cc6b243c150ae46c8db55e3a74fd4a19b46bc
Jhonatan Catabriga <Catabriga@Uandersons-Mac-mini.local>
2015-07-19 17:33:36 -0300

    Ajusta conexão mongo

--- service/build/mongooseConnection.js
+++ service/build/mongooseConnection.js
@@ -9,10 +9,18 @@ var _mongoose2 = _interopRequireDefault(_mongoose);
 //mongodb://usuar:senha@host:port
 var usuario = '';
 var senha = '';
-var porta = '';
 var host = 'localhost';
-var base = 'goBike';
-var stringConnection = 'mongodb://' + usuario + senha + host + porta + '/' + base;
+var base = 'gobike';
+
+if (process.env.PRODUCTION) {
+  usuario = 'gobikemaster';
+  senha = ':bike!';
+  host = '@gobike.herokuapp.com:59722';
+}
+
+var stringConnection = 'mongodb://' + usuario + senha + host + '/' + base;
+
+console.log(stringConnection);
 _mongoose2['default'].connect(stringConnection);
 
 var db = _mongoose2['default'].connection;
