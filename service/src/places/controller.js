e30dc97852cf6f7fc589ecb338dfc593f1749f1a
Jhonatan Catabriga <Catabriga@Uandersons-Mac-mini.local>
2015-07-29 14:44:15 -0300

    Ajuste

--- service/src/places/controller.js
+++ service/src/places/controller.js
@@ -4,15 +4,15 @@ import api from '../api'
 
 var sendResponse = api.sendResponse;
 
-const find = sendResponse(req =>
-  Place.find(req.query));
+const find = sendResponse(req => Place.find(req.query));
 
 const create = sendResponse((req, res) =>{
   res.status(201);
   var place = new Place({
     coordenates: req.body
   });
-  return new Promise((resolve, reject) => place.save().then(resolve, reject));
+  return new Promise((resolve, reject) =>
+    place.save().then(resolve, reject));
 });
 
 export default {
